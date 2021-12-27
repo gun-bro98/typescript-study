fed27f8ff88996433e899478b0f40e3e913ed30d
gunhyung <one0374@naver.com>
2021-12-27 21:30:34 +0900

    싱글톤 패턴 - private 생성자

---
+++ cl-interfaces-01-starting-setup/src/classes.ts
@@ -0,0 +1,117 @@
+abstract class Department {
+  static fiscalYear = "2021";
+  protected employees: string[] = [];
+  constructor(protected readonly id: string, public name: string) {
+    this.name = name;
+    this.id = id;
+  }
+
+  static createEmployee(name: string) {
+    return { name: name };
+  }
+
+  abstract describe(this: Department): void;
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+class ITDeparment extends Department {
+  // admins: string[];
+  constructor(id: string, public admins: string[]) {
+    super(id, "IT");
+    // this.admins = admins; //딱히 쓸 필요가 없다. 매개변수 설정할 때 이미 선언을 한 것
+  }
+  describe() {
+    console.log("IT Department - ID " + this.id);
+  }
+}
+
+class AccountingDeparment extends Department {
+  private lastReport: string;
+  private static instance: AccountingDeparment;
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("No report found.");
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("please pass in a valid value!");
+    }
+    this.addReport(value);
+  }
+
+  private constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+    this.lastReport = reports[0];
+  }
+
+  static getInstance() {
+    if (AccountingDeparment.instance) {
+      return this.instance;
+    }
+    this.instance = new AccountingDeparment("d2", []);
+    return this.instance;
+  }
+
+  describe() {
+    console.log("Accounting Department - ID: " + this.id);
+  }
+
+  addEmployee(name: string) {
+    if (name === "Max") {
+      return;
+    }
+    this.employees.push(name);
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+    this.lastReport = text;
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const employee1 = Department.createEmployee("Max");
+console.log(employee1, Department.fiscalYear);
+
+const it = new ITDeparment("d1", ["Max"]);
+it.addEmployee("Max");
+it.addEmployee("Manu");
+
+it.describe();
+it.name = "New Name";
+it.printEmployeeInformation();
+
+console.log(it);
+
+const accounting = AccountingDeparment.getInstance();
+const accounting2 = AccountingDeparment.getInstance();
+console.log(accounting);
+console.log(accounting2);
+
+accounting.mostRecentReport = "Year End Project";
+accounting.addReport("Something went wrong...");
+console.log(accounting.mostRecentReport);
+
+accounting.addEmployee("박건형");
+accounting.addEmployee("Max");
+
+// accounting.printReports();
+// accounting.printEmployeeInformation();
+accounting.describe();
+
+// const accoutingCopy = { name: "ss", describe: accounting.describe };
+// accoutingCopy.describe();
--- cl-interfaces-01-starting-setup/src/practice.ts
+++ cl-interfaces-01-starting-setup/src/practice.ts
@@ -1,15 +1,41 @@
-class Animal {
-  kind: string;
+abstract class Animal {
+  protected kind: string;
   constructor(kind: string) {
     this.kind = kind;
   }
-  beBorn(this: Animal) {
-    console.log(this.kind);
+  abstract beBorn(this: Animal): void;
+
+  get currentKind() {
+    return this.kind;
+  }
+
+  set currentKind(value: string) {
+    if (!value) {
+      throw new Error("No Parameter");
+    }
+    this.kind = value;
   }
 }
 
-const cat = new Animal("Mammalia");
-const cat2 = { kind: "Mammalia", beBorn: cat.beBorn };
+class Cat extends Animal {
+  constructor(kind: string, public name: string) {
+    super(kind);
+  }
+
+  printName() {
+    console.log(`이 ${this.kind}의 이름은 ${this.name}`);
+  }
+
+  beBorn() {
+    console.log(this.kind + "로 태어났다.");
+  }
+}
+const cat = new Cat("고양이", "고양희");
+cat.printName();
 cat.beBorn();
+console.log(cat.currentKind);
+
+// const cat = new Animal("Mammalia");
+
 //결과
 //Mammalia
