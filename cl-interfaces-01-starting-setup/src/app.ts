0665563c102662a06af3b6bba32dc1dd18b4c894
gunhyung <one0374@naver.com>
2021-12-27 17:38:39 +0900

    protected and override

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -1,6 +1,6 @@
 class Department {
   // name: string;
-  private employees: string[] = [];
+  protected employees: string[] = [];
   constructor(private readonly id: string, public name: string) {
     this.name = name;
     this.id = id;
@@ -20,15 +20,57 @@ class Department {
   }
 }
 
-const accounting = new Department("d1", "Accounting");
+class ITDeparment extends Department {
+  // admins: string[];
+  constructor(id: string, public admins: string[]) {
+    super(id, "IT");
+    // this.admins = admins; //딱히 쓸 필요가 없다. 매개변수 설정할 때 이미 선언을 한 것
+  }
+}
+
+class AccountingDeparment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+  }
+
+  addEmployee(name: string) {
+    if (name === "Max") {
+      return;
+    }
+    this.employees.push(name);
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const it = new ITDeparment("d1", ["Max"]);
+it.addEmployee("Max");
+it.addEmployee("Manu");
+
+it.describe();
+it.name = "New Name";
+it.printEmployeeInformation();
+
+console.log(it);
+
+const accounting = new AccountingDeparment("d2", []);
+
+accounting.addReport("Something went wrong...");
+
+accounting.addEmployee("박건형");
 accounting.addEmployee("Max");
-accounting.addEmployee("Manu");
 
-// accounting.employees[2] = "Anna";
-// accounting.id = 'd5';
+accounting.printReports();
 
-accounting.describe();
 accounting.printEmployeeInformation();
+
+console.log(accounting);
 
 // const accoutingCopy = { name: "ss", describe: accounting.describe };
 // accoutingCopy.describe();
