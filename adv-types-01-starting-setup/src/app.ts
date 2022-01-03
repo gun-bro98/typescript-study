6ce14d7d42adcd1c5d2f407d75a0a9c92acef15d
gunhyung <one0374@naver.com>
2022-01-03 21:06:06 +0900

    discriminated Unions: 유니온과 스위치문 사용

--- adv-types-01-starting-setup/src/app.ts
+++ adv-types-01-starting-setup/src/app.ts
@@ -69,3 +69,36 @@ function useVehicle(vehicle: Vehicle) {
     vehicle.loadCargo(1000);
   }
 }
+useVehicle(v1);
+useVehicle(v2);
+
+interface Bird {
+  type: "bird";
+  flyingSpeed: number;
+}
+
+interface Hourse {
+  type: "hourse";
+  runningSpeed: number;
+}
+
+type Animal = Bird | Hourse;
+
+function moveAnimal(animal: Animal) {
+  let speed;
+  // 인터페이스는 in 키워드를 사용해서 타입가드 처럼 쓸 수가 있긴하다.
+  // 다만 스트링 형태로 적는거라 틀릴 수도 있다.
+  // 그러니 discriminated Unions이라는 명칭이지만 스위치 문 사용을 뜻한다.
+  // 인터페이스말고도 다른 객체들도 사용가능
+  // 이것은 if문 양이 많을 때 사용하면 된다.
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+    case "hourse":
+      speed = animal.runningSpeed;
+      break;
+  }
+  console.log("달리는 속도" + speed);
+}
+moveAnimal({ type: "bird", flyingSpeed: 30 });
