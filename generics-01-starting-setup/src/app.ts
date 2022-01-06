0dc48a61816a6b064adcace7b0578341929e92b6
gunhyung <one0374@naver.com>
2022-01-06 17:13:02 +0900

    keyof 제약조건2

--- generics-01-starting-setup/src/app.ts
+++ generics-01-starting-setup/src/app.ts
@@ -47,3 +47,12 @@ function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
 }
 
 console.log(countAndDescribe(["sports", "hiking"]));
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return obj[key];
+}
+
+extractAndConvert({ name: "gunbro" }, "name");
