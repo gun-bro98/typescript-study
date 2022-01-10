574864a9bf1f6c0fcb4195b1d047e6cc12a735fb
gunhyung <one0374@naver.com>
2022-01-10 19:53:20 +0900

    decorator factory

--- decorators-01-starting-setup/src/app.ts
+++ decorators-01-starting-setup/src/app.ts
@@ -1,13 +1,17 @@
 //데코레이터는 함수이다.
-
-function Logger(constructor: Function) {
-  console.log("Logging");
-  console.log(constructor);
+//데코레이터 팩토리 => 데코레이터를 반환하는 함수
+function Logger(logString: string) {
+  // 반환되는 내부 데코레이터 함수가 사용하는 값을 전달 할 수 있다는 이점이
+  //있다.
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
 }
 //@은 타입스크립트에서 인식하는 특별한 식별자
 //@뒤에는 함수가 나오는데 이건 실행이 아니라 지목이다.
 //자바스크립트가 클래스 정의, 생성자 함수 정의를 찾으면 데코레이터가 실행됨
-@Logger
+@Logger("LOGGING - PERSON")
 class Person {
   name = "Max";
 
