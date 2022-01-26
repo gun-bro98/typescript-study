590a335b1bdeae37373076b63965a4fb2340454d
gunhyung <one0374@naver.com>
2022-01-26 17:07:21 +0900

    dom 요소와 상호 작용

---
+++ prj-00-initial-starting-setup/src/app.ts
@@ -0,0 +1,41 @@
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
+
+  constructor() {
+    this.templateElement = document.getElementById("project-input")! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(this.templateElement.content, true);
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+
+    this.titleInputElement = this.element.querySelector("#title") as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector("#description") as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector("#people") as HTMLInputElement;
+    
+    this.configure();
+    this.attach();
+  }
+
+  private submitHandler(event: Event){
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+  // submitHandler 메소드를 이벤트 리스너에 바인드히면, 실행되는 해당 매소드는 다른 무언가로 바인드되는 this가 있어야 한다.
+  // 여기서 this와 submitHandler this와 같은 클래스를 가리키게 된다.
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
